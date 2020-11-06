991ea92204acf5204b046dd4c36ac61f64fb09c2
pierrehlvt <pierre.halavent@gmail.com>
2020-11-06 14:58:37 +0100

    Adding message Dialog to confirm login

--- src/main/java/com/orbisbank/gui/Login.java
+++ src/main/java/com/orbisbank/gui/Login.java
@@ -32,21 +32,14 @@ public class Login extends JFrame {
                     user = DaoFactory.getUsersDao().getUsersByEmail(email);
                     if (hash.hashPassword(password).equals(user.getPassword())) {
                         System.out.println(" Hello " + user.getUsers_surname() + " " + user.getUsers_name());
+                        JOptionPane.showMessageDialog(loginPanel, " Hello " + user.getUsers_surname() + " " + user.getUsers_name());
                     } else {
                         System.out.println("Wrong email or password");
+                        JOptionPane.showMessageDialog(loginPanel, "Wrong email or password");
                     }
                 } catch (Exception exception) {
                     exception.printStackTrace();
                 }
-            }
-        });
-    }
-
-    public static void main(String[] args) {
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                Login loginForm = new Login();
-                loginForm.setVisible(true);
             }
         });
     }
