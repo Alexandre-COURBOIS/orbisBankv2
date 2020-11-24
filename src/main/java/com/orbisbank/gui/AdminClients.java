6980883d8520c8ef9b872bc200ca318f59a0268e
Yann Poher <yan.poher@gmail.com>
2020-11-24 10:31:31 +0100

    initialization of admin client page

---
+++ src/main/java/com/orbisbank/gui/AdminClients.java
@@ -0,0 +1,35 @@
+package com.orbisbank.gui;
+
+import javax.swing.*;
+
+public class AdminClients {
+    private JPanel adminPanel;
+    private JPanel divLeft;
+    private JButton utilisateursButton;
+    private JButton commerciauxButton;
+    private JButton clientsButton;
+    private JPanel divLogoTop;
+    private JPanel divRight;
+    private JPanel divTitle;
+    private JPanel Log;
+    private JLabel log;
+    private JPanel ButtonLogout;
+    private JButton buttonLogout;
+    private JPanel TitleAdmin;
+    private JLabel titleAdmin;
+    private JPanel divUser;
+    private JPanel divPhoto;
+    private JLabel photo;
+    private JPanel divName;
+    private JLabel name;
+    private JLabel stats;
+    private JPanel divEdit;
+    private JLabel edit;
+    private JLabel delete;
+    private JLabel contact;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JPanel divId;
+    private JPanel divContent;
+}
